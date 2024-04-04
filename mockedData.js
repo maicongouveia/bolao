9dfa502f2bee155483288603bd02e7348dc0a0d3
Maicon Gouveia <gouveia.maicon@gmail.com>
2024-04-04 12:45:52 -0300

    feat: mocked data file

---
+++ mockedData.js
@@ -0,0 +1,41 @@
+let score = 86;
+
+let apostasUmGanhadorAproximado = {
+  "Alan":81,
+  "Alex":84,
+  "Arthur":0,
+  "Daniel":83,
+  "Gruber":89,
+  "Loren":80,
+  "Zé":82,
+}
+
+let apostasDoisGanhadoresAproximado = {
+  "Alan":81,
+  "Alex":84,
+  "Arthur":76,
+  "Daniel":83,
+  "Gruber":89,
+  "Loren":80,
+  "Zé":82,
+}
+
+let apostasComSnipada = {
+  "Alan":81,
+  "Alex":78,
+  "Arthur":82,
+  "Daniel":80,
+  "Gruber":86,
+  "Loren":85,
+  "Zé":77
+}
+
+let apostasNinguemAcertou = {
+  "Alan":60,
+  "Alex":84,
+  "Arthur":98,
+  "Daniel":63,
+  "Gruber":89,
+  "Loren":99,
+  "Zé":62,
+}

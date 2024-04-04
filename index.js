398d1e427ed05b621b6d8fc7e42443fb0811c0f6
Maicon Gouveia <gouveia.maicon@gmail.com>
2024-04-04 12:45:38 -0300

    feat: remove mocked data

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-let apostasUmGanhadorAproximado = {
-  "Alan":81,
-  "Alex":84,
-  "Arthur":0,
-  "Daniel":83,
-  "Gruber":89,
-  "Loren":80,
-  "Zé":82,
-}
-
-let apostasDoisGanhadoresAproximado = {
-  "Alan":81,
-  "Alex":84,
-  "Arthur":76,
-  "Daniel":83,
-  "Gruber":89,
-  "Loren":80,
-  "Zé":82,
-}
-
-let apostasComSnipada = {
-  "Alan":81,
-  "Alex":78,
-  "Arthur":82,
-  "Daniel":80,
-  "Gruber":86,
-  "Loren":85,
-  "Zé":77
-}
-
-let apostasNinguemAcertou = {
-  "Alan":60,
-  "Alex":84,
-  "Arthur":98,
-  "Daniel":63,
-  "Gruber":89,
-  "Loren":99,
-  "Zé":62,
-}
+const getDataFromGoogleSheetAPI = require('./getData.js');
 
 function calcularDistacia(apostas, pontuacao){
   let distancia = {};
@@ -133,8 +95,6 @@ function montarMensagem(ganhadores){
 
   return mensagem;
 }
-
-let pontuacao = 86;
 
 function calcularVencedorDoDia(day, apostas, pontuacao){
   

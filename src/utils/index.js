d4d7b0cbd984f6ca5b0f3fdf4d5da8817e66aafc
Maicon Gouveia <gouveia.maicon@gmail.com>
2024-04-23 00:10:05 -0300

    fix: format date for server

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 const getDate = () => {
   let now = new Date();
-  now = now.toLocaleString({timeZone: "America/Sao_Paulo"}).split("/");  
+  now = now.toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"}).split("/");
   let date = now[0]+"/"+now[1];
   //console.log(date);
   return date;

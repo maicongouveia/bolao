ecdf8be94399551370d6cf105ef1a692293d12a5
Maicon Gouveia <gouveia.maicon@gmail.com>
2024-04-22 21:16:43 -0300

    feat: set timezone of date

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,8 @@
 const getDate = () => {
   let now = new Date();
-  now = now.toISOString().split("T")[0].split("-");
-  let date = now[2]+"/"+now[1];
+  now = now.toLocaleString({timeZone: "America/Sao_Paulo"}).split("/");  
+  let date = now[0]+"/"+now[1];
+  //console.log(date);
   return date;
 }
 

1d0d4df921341445792f896269cd4e7a575ea4f3
Maicon Gouveia <gouveia.maicon@gmail.com>
2024-05-04 16:26:19 -0300

    refactor: data service

--- src/data/data.service.js
+++ src/data/data.service.js
@@ -3,34 +3,9 @@ const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
 const filePath = appDir + "/../credentials.json";
 
-const getAllData = async() => {
+const spreadsheetId = "15maJIAB28avJGYQ-03u1b4C_QmcOtQioHPWcEolVors";
 
-    const auth = new google.auth.GoogleAuth({
-      keyFile: filePath,
-      scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
-    
-    // Create client instance for auth
-    const client = await auth.getClient();
-    
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: "v4", auth: client });
-    
-    const spreadsheetId = "15maJIAB28avJGYQ-03u1b4C_QmcOtQioHPWcEolVors";
-
-    const getRows = await googleSheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: 'Maio!B1:AG9',
-      majorDimension: "COLUMNS"
-    });
-
-    let data = getRows.data.values;
-
-    return data;
-  
-}
-
-const setData = async(values) => {
+const googleSheetsClient = async() => {
   const auth = new google.auth.GoogleAuth({
     keyFile: filePath,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -42,7 +17,32 @@ const setData = async(values) => {
   // Instance of Google Sheets API
   const googleSheets = google.sheets({ version: "v4", auth: client });
   
-  const spreadsheetId = "15maJIAB28avJGYQ-03u1b4C_QmcOtQioHPWcEolVors";
+  return googleSheets
+}
+
+const getAllData = async(majorDimension = null) => {
+
+  if(!majorDimension) {
+    majorDimension = "COLUMNS";
+  }
+
+  const googleSheets = await googleSheetsClient();
+
+  const getRows = await googleSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: 'Maio!B1:AG9',
+    majorDimension
+  });
+
+  let data = getRows.data.values;
+
+  return data;
+  
+}
+
+const setData = async(values) => {
+
+  const googleSheets = await googleSheetsClient();
 
   const body = {
     values,
